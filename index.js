16d481be4b39a62689ae7db8676ff757caea0461
Alex Muramoto <amuramoto@apache.org>
2017-12-07 16:22:31 -0800

    adds get traffic

--- index.js
+++ index.js
@@ -30,30 +30,76 @@ mongodb.connect(mongodb_url, (err, mongo_client) => {
 
 function getRepoData(org, repo) {
 
-  let request_options = {
-    "url": `${github_api_base_url}/repos/${org}/${repo}`,
-    "method": "GET",
-    "headers": {
-      "User-agent": "Github Stats Client"
+  let document = {}
+  document[repo] = {};
+  let repo_data = {};
+  let stats = getStats(org, repo);   
+  let traffic = getTraffic(org, repo);
+
+  Promise.all([stats, traffic]).then(values => {
+    Object.assign(document[repo], values[0], values[1]);
+console.log('ok')
+console.log(document[repo])
+  });
+
+    // writeDocument(document);    
+  
+
+}
+
+function getStats (org, repo) {
+  return new Promise((resolve, reject) => {
+    let request_options = {
+      "url": `${github_api_base_url}/repos/${org}/${repo}`,
+      "method": "GET",
+      "headers": {
+        "User-agent": "Github Stats Client"
+      }
     }
-  }
 
-  request(request_options, (err, res, body) => {
+    request(request_options, (err, res, body) => {
 
-    body = JSON.parse(body);
-    
-    let stars = body.stargazers_count;
-    let forks = body.forks_count;
-    let watchers = body.subscribers_count;
-    let document = {};
+      body = JSON.parse(body);
+      
+      let stars = body.stargazers_count;
+      let forks = body.forks_count;
+      let watchers = body.subscribers_count;
+      let stats =  {
+        "watchers": watchers,
+        "stars": stars,
+        "forks": forks          
+      };
 
-    document[repo] = {
-      "watchers": watchers,
-      "stars": stars,
-      "forks": forks          
-    };
-    
-    writeDocument(document);    
+      resolve(stats);
+    });
+  });
+
+}
+
+function getTraffic (org, repo) {
+  return new Promise((resolve, reject) => {
+
+
+    let token = env.github_token;
+    let request_options = {
+      "url": `${github_api_base_url}/repos/${org}/${repo}/traffic/views?per=day`,
+      "method": "GET",
+      "headers": {
+        "User-agent": "Github Stats Client",
+        'Authorization': 'token ' + token 
+      }
+    }
+
+    request(request_options, (err, res, body) => {
+      body = JSON.parse(body);
+      let views = {
+        'views': {
+          'total': body.views[body.views.length-2].count,
+          'unique': body.views[body.views.length-2].uniques
+        }        
+      }
+      resolve(views);
+    })
   })
 
 }
